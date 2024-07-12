c07f188e166cb53078e57dbc9239402ce834d489
BuildTools <legonexoknights123@gmail.com>
2024-07-12 09:56:48 +0200

    Added basic implementation for adding menu

---
+++ src/main/java/com/mineshaft/mineshaftapi/listener/UIListener.java
@@ -0,0 +1,44 @@
+/*
+ *     This program is a Minecraft plugin developed in Java for the Spigot API.
+ *     It adds multiple RPG features intended for Multiplayer gameplay.
+ *
+ *     Copyright (C) 2024  Sebastian Frynas
+ *
+ *     This program is free software: you can redistribute it and/or modify
+ *     it under the terms of the GNU General Public License as published by
+ *     the Free Software Foundation, either version 3 of the License, or
+ *     (at your option) any later version.
+ *
+ *     This program is distributed in the hope that it will be useful,
+ *     but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *     GNU General Public License for more details.
+ *
+ *     You should have received a copy of the GNU General Public License
+ *     along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ *
+ *
+ */
+
+package com.mineshaft.mineshaftapi.listener;
+
+import org.bukkit.ChatColor;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.inventory.InventoryClickEvent;
+
+public class UIListener implements Listener {
+
+    @EventHandler
+    void onClick(InventoryClickEvent e) {
+
+        String title = ChatColor.translateAlternateColorCodes('&', e.getView().getTitle());
+
+        String menu = ChatColor.BLACK + "Menu";
+
+        if (title.equalsIgnoreCase(ChatColor.BLACK + "Menu")) {
+            e.setCancelled(true);
+        }
+    }
+
+}
---
+++ src/main/java/com/mineshaft/mineshaftapi/manager/ui/PlayerMenuManager.java
@@ -0,0 +1,40 @@
+/*
+ *     This program is a Minecraft plugin developed in Java for the Spigot API.
+ *     It adds multiple RPG features intended for Multiplayer gameplay.
+ *
+ *     Copyright (C) 2024  Sebastian Frynas
+ *
+ *     This program is free software: you can redistribute it and/or modify
+ *     it under the terms of the GNU General Public License as published by
+ *     the Free Software Foundation, either version 3 of the License, or
+ *     (at your option) any later version.
+ *
+ *     This program is distributed in the hope that it will be useful,
+ *     but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *     GNU General Public License for more details.
+ *
+ *     You should have received a copy of the GNU General Public License
+ *     along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ *
+ *
+ */
+
+package com.mineshaft.mineshaftapi.manager.ui;
+
+import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
+import org.bukkit.entity.Player;
+import org.bukkit.inventory.Inventory;
+
+public class PlayerMenuManager {
+
+    public static void OpenPlayerMenu(Player player) {
+
+        Inventory ui = Bukkit.createInventory(null, 27, ChatColor.BLACK + "Menu");
+
+        player.openInventory(ui);
+
+    }
+
+}

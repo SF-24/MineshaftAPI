e4af67d8c5648fb0a5e72aa10b574cbefae18fa2
SF-24 <legonexoknights123@gmail.com>
2025-04-30 17:43:24 +0100

    Added string manager

---
+++ src/main/java/com/mineshaft/mineshaftapi/manager/StringManager.java
@@ -0,0 +1,74 @@
+/*
+ * Copyright (c) 2025. Sebastian Frynas
+ *
+ *     This program is free software: you can redistribute it and/or modify
+ *     it under the terms of the GNU Affero General Public License as
+ *     published by the Free Software Foundation, either version 3 of the
+ *     License, or (at your option) any later version.
+ *
+ *     This program is distributed in the hope that it will be useful,
+ *     but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *     GNU Affero General Public License for more details.
+ *
+ *     You should have received a copy of the GNU Affero General Public License
+ *     along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ *
+ */
+
+package com.mineshaft.mineshaftapi.manager;
+
+import io.lumine.mythic.bukkit.utils.text.Text;
+import net.md_5.bungee.api.ChatMessageType;
+import net.md_5.bungee.api.chat.TextComponent;
+import org.bukkit.ChatColor;
+import org.bukkit.entity.Player;
+
+public class StringManager {
+    private final static int CENTER_PX = 154;
+
+    public static void sendActionBar(Player player, String text) {
+        player.spigot().sendMessage(ChatMessageType.ACTION_BAR, TextComponent.fromLegacyText(text));
+    }
+
+    public static void sendCenteredMessage(Player player, String message){
+        if(message == null || message.equals("")) player.sendMessage("");
+        message = ChatColor.translateAlternateColorCodes('&', message);
+
+        int messagePxSize = 0;
+        boolean previousCode = false;
+        boolean isBold = false;
+
+        for(char c : message.toCharArray()){
+            if(c == '§'){
+                previousCode = true;
+            }else if(previousCode == true){
+                previousCode = false;
+                if(c == 'l' || c == 'L'){
+                    isBold = true;
+                }else isBold = false;
+            }else{
+                Text.DefaultFontInfo dFI = Text.DefaultFontInfo.getDefaultFontInfo(c);
+                messagePxSize += isBold ? dFI.getBoldLength() : dFI.getLength();
+                messagePxSize++;
+            }
+        }
+
+        int halvedMessageSize = messagePxSize / 2;
+        int toCompensate = CENTER_PX - halvedMessageSize;
+        int spaceLength = Text.DefaultFontInfo.SPACE.getLength() + 1;
+        int compensated = 0;
+        StringBuilder sb = new StringBuilder();
+        while(compensated < toCompensate){
+            sb.append(" ");
+            compensated += spaceLength;
+        }
+        player.sendMessage(sb + message);
+    }
+
+    public static void debugMessage(Player player, String string) {
+        if(player.hasPermission("mineshaft.debug")) {
+            player.sendMessage(ChatColor.RED + "[DEBUG] " + string);
+        }
+    }
+}

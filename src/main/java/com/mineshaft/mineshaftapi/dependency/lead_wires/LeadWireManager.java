a777fa58ea3937771519815b15e551c3aa366ced
SF-24 <legonexoknights123@gmail.com>
2025-08-06 18:16:33 +0200

    Added LeadWire Api support and better item management

--- src/main/java/com/mineshaft/mineshaftapi/dependency/lead_wires/LeadWireManager.java
+++ src/main/java/com/mineshaft/mineshaftapi/dependency/lead_wires/LeadWireManager.java
@@ -2,16 +2,16 @@
  * Copyright (c) 2025. Sebastian Frynas
  *
  *     This program is free software: you can redistribute it and/or modify
- *     it under the terms of the GNU Affero General Public License as
+ *     it under the terms of the GNU Affero General Public static License as
  *     published by the Free Software Foundation, either version 3 of the
  *     License, or (at your option) any later version.
  *
  *     This program is distributed in the hope that it will be useful,
  *     but WITHOUT ANY WARRANTY; without even the implied warranty of
  *     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *     GNU Affero General Public License for more details.
+ *     GNU Affero General Public static License for more details.
  *
- *     You should have received a copy of the GNU Affero General Public License
+ *     You should have received a copy of the GNU Affero General Public static License
  *     along with this program.  If not, see <https://www.gnu.org/licenses/>.
  *
  */
@@ -22,6 +22,7 @@ import com.mineshaft.mineshaftapi.util.LocationUtil;
 import me.saharnooby.plugins.leadwires.LeadWires;
 import me.saharnooby.plugins.leadwires.api.LeadWiresAPI;
 import me.saharnooby.plugins.leadwires.wire.Wire;
+import org.bukkit.Bukkit;
 import org.bukkit.Location;
 import org.bukkit.util.Vector;
 
@@ -38,11 +39,11 @@ public class LeadWireManager {
         return getApi().getWires().get(uuid);
     }
 
-    public boolean wireHasEndAtLocation(UUID uuid, Location loc) {
+    public static boolean wireHasEndAtLocation(UUID uuid, Location loc) {
         return getWire(uuid).getWorld().equals(loc.getWorld().getName()) && ( getRoundedEndA(uuid).equals(LocationUtil.getRoundedLocation(loc)) || getRoundedEndB(uuid).equals(LocationUtil.getRoundedLocation(loc)));
     }
 
-    public Vector getOtherEnd(UUID uuid, Location loc) {
+    public static Vector getOtherEnd(UUID uuid, Location loc) {
         if(wireHasEndAtLocation(uuid, loc)) {
             if(getRoundedEndA(uuid).equals(LocationUtil.getRoundedLocation(loc))) {
                 return getRoundedEndB(uuid);
@@ -53,17 +54,46 @@ public class LeadWireManager {
         return null;
     }
 
-    public ArrayList<Vector> findCableEnds(UUID uuid, Location loc) {
+    public static Location getOtherEndAsLocation(UUID uuid, Location loc) {
+        if(getOtherEnd(uuid, loc)==null || Bukkit.getWorld(getWire(uuid).getWorld())==null) return null;
+        return getOtherEnd(uuid, loc).toLocation(Bukkit.getWorld(getWire(uuid).getWorld()));
+    }
+
+    public static ArrayList<Vector> findCableEnds(UUID uuid, Location loc) {
         ArrayList<Vector> cableEnds = new ArrayList<>();
         // TODO: Find cable ends
         return cableEnds;
     }
 
-    public Vector getRoundedEndA(UUID uuid) {
+    public static ArrayList<UUID> findConnectorsAtPoint(UUID uuid, Location loc) {
+        ArrayList<UUID> connectors = new ArrayList<>();
+        for(UUID element : getApi().getWires().keySet()) {
+            if(!element.equals(uuid) && wireHasEndAtLocation(element, loc)) {
+                connectors.add(element);
+            }
+        }
+        return connectors;
+    }
+
+    public static ArrayList<UUID> findConnectorsAtOtherEnd(UUID uuid, Location loc) {
+        return findConnectorsAtPoint(uuid, getOtherEndAsLocation(uuid, loc));
+    }
+
+    public static ArrayList<UUID> getWires(Location loc) {
+        ArrayList<UUID> wires = new ArrayList<>();
+        for(UUID element : getApi().getWires().keySet()) {
+            if(wireHasEndAtLocation(element, loc)) {
+                wires.add(element);
+            }
+        }
+        return wires;
+    }
+
+    public static Vector getRoundedEndA(UUID uuid) {
         return new Vector(getWire(uuid).getA().getBlockX(),getWire(uuid).getA().getBlockY(),getWire(uuid).getA().getBlockZ());
     }
 
-    public Vector getRoundedEndB(UUID uuid) {
+    public static Vector getRoundedEndB(UUID uuid) {
         return new Vector(getWire(uuid).getB().getBlockX(),getWire(uuid).getB().getBlockY(),getWire(uuid).getB().getBlockZ());
     }
 
